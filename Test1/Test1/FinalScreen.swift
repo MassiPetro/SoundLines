fa6223b7de96a2bc352b91c87859450cb16c8014
fedepini <fpini95@gmail.com>
2019-08-26 22:49:42 +0200

    Created graphic on all screens. Added final screen.

---
+++ Test1/Test1/FinalScreen.swift
@@ -0,0 +1,18 @@
+//
+//  FinalScreen.swift
+//  Test1
+//
+//  Created by Fede on 26/08/19.
+//  Copyright © 2019 Comelicode. All rights reserved.
+//
+
+import UIKit
+
+class FinalScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
