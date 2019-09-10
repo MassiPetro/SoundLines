2072da5341c3f721e82fb35f100c99d3baf29cfb
fedepini <fpini95@gmail.com>
2019-09-10 12:00:55 +0200

    Updated view elements on not-game-levels, position and dimension now depend on screen size

--- Test1/Test1/Level1Screen.swift
+++ Test1/Test1/Level1Screen.swift
@@ -16,11 +16,24 @@ class Level1Screen: UIViewController {
         self.navigationItem.setHidesBackButton(true, animated:true);
         self.navigationController?.navigationBar.isHidden = true;
         
+        // Places view elements on the screen: title, text and button
+        // they are centered vertically and their dimension depends on
+        // screen width and height
+        
+        let screenWidth = view.frame.width
         let screenHeight = view.frame.size.height
         
+        screenTitle.sizeToFit()
+        screenTitle.frame = CGRect(x: 0, y: 0, width: screenWidth * 0.8, height: screenHeight * 0.2)
         screenTitle.center.x = self.view.center.x
         screenTitle.center.y = self.view.center.y - screenHeight * 0.3
+        
+        screenText.sizeToFit()
+        screenText.frame = CGRect(x: 0, y: 0, width: screenWidth * 0.8, height: screenHeight * 0.4)
         screenText.center = self.view.center
+        
+        screenButton.sizeToFit()
+        screenButton.frame = CGRect(x: 0, y: 0, width: screenWidth * 0.3, height: screenHeight * 0.2)
         screenButton.center.x = self.view.center.x
         screenButton.center.y = self.view.center.y + screenHeight * 0.3
         
